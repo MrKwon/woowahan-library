d46652b06892da0e999284e212f757ad4ad5238b
MrKwon <erased11@gmail.com>
2019-08-08 19:36:31 +0900

    [manage] EDIT : layout and edit ui ux

--- manage/src/views/RentStatus.vue
+++ manage/src/views/RentStatus.vue
@@ -4,23 +4,30 @@
       <v-toolbar class="book_list_toolbar black" flat dense dark>
         <v-toolbar-title>대여 현황</v-toolbar-title>
         <v-spacer></v-spacer>
+        <v-btn v-on:click="_refresh()">
+          새로고침
+        </v-btn>
       </v-toolbar>
       <RentStatusTitle />
+      <v-layout v-if="rentStatus.length === 0" justify-center align-center style="height: 300px; text-align: center;">
+        <div>
+          No Items
+        </div>
+      </v-layout>
       <div
         v-for="(rent, i) in rentStatus"
         :key="i"
       >
         <RentStatusItem :rent="rent"/>
       </div>
-      <div class="text-xs-center">
-        <v-pagination
-          v-model="page"
-          :length="length"
-        ></v-pagination>
-      </div>
-      <v-btn @click="getRentStatus">
-        테스트용 버튼
-      </v-btn>
+      <v-layout row justify-center align-center pa-2>
+        <v-btn fab small flat dark class="black" @click="_pageDownBtnHandler">
+          <v-icon>keyboard_arrow_left</v-icon>
+        </v-btn>
+        <v-btn fab small flat dark class="black" @click="_pageUpBtnHandler">
+          <v-icon>keyboard_arrow_right</v-icon>
+        </v-btn>
+      </v-layout>
     </v-layout>
  </v-container>
 </template>
@@ -60,6 +67,22 @@ export default {
       } catch (error) {
         console.log(error)
       }
+    },
+
+    _pageUpBtnHandler() {
+      if (this.rentStatus.length === 10) {
+        this.page += 1
+      }
+    },
+
+    _pageDownBtnHandler() {
+      if (this.page > 1) {
+        this.page -= 1
+      }
+    },
+
+    _refresh() {
+      this.getRentStatus()
     }
   },
 
